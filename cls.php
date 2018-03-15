ddc955841b17028cd0fa87a3684168912fda59f0
swathikundella <35362116+swathikundella@users.noreply.github.com>
2018-03-15 11:15:35 +0530

    Add files via upload

---
+++ cls.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Class Example </title>
+		<style rel="stylesheet" type="text/css">
+		     .Divcommon{
+						width:95%;
+						margin-left:30px;
+						}
+			 .div1{background-color:#27D3C3;
+					font-size:40px;
+					color:#E7FFFF;
+					text-align:center;
+					height:100px;
+					width:95%;
+					padding-top:30px;					
+					
+						}
+			 .div2{ 
+					height:50px;
+					background-color:#FFFFFF;
+					margin-top:5px;
+					float
+						}
+			 .div3{		width:45%;
+					color:#27D3C3;
+					font-size:20px;	
+					text-align:center-left;
+					margin-left:10px;
+					margin-top:10px;
+					float:left;
+					
+					
+			 }
+			 .div4{		width:50%;
+					background-color:#EFEFEF;
+					font-size:20px;	
+					align:right;
+					height:30px;
+					margin-left:10px;
+					margin-top:10px;
+					float:left;
+					
+					
+			 }
+			 
+			 .subdiv1{
+					width:24.8%;
+					background-color:#fff;
+					height:300px;
+					float:left;
+					margin-left:28px;
+					margin-top:10px;
+					
+			 }
+			 .subdiv2{
+					width:40%;
+					background-color:#27D3C3;
+					height:300px;
+					float:left;
+					margin-left:45px;
+					margin-top:10px;
+					
+					
+			 }
+			 .subdiv3{
+					width:45%;
+					background-color:#fff;
+					margin-left:10px;
+					height:40px;
+					float:left;
+			 
+			 }
+			 .subdiv4{
+					width:45%;
+					background-color:#fff;
+					height:40px;
+					margin-left:10px;
+					float:left;
+			 
+			 }
+			 .subdiv5{
+					width:92%;
+					background-color:#fff;
+					height:200px;
+					margin-left:10px;
+					margin-top:50px;
+					clear:left;
+			 
+			 }
+			 .subdiv6{
+					width:92%;
+					background-color:#fff;
+					height:40px;
+					margin-left:10px;
+					margin-top:10px;
+					clear:left;
+			 
+			 }
+			 .subdiv7{
+					width:28.7%;
+					background-color:#fff;
+					height:40px;
+					padding-left:20px;
+					margin-left:30px;
+					margin-top:15px;
+					float:left;
+					margin-bottom:10px;
+			 
+			 }
+			 .subdiv8{
+					width:95%;
+					background-color:#fff;
+					height:40px;
+					margin-left:30px;
+					clear:left;
+					margin-top:10px;
+			 
+			 }
+			 .subdiv9{
+					width:30%;
+					background-color:#EFEFEF;
+					height:70px;
+					margin-left:10px;					
+					margin-top:5px;
+			 
+			 }
+			 .subdiv10{
+					width:80%;
+					background-color:#EFEFEF;
+					height:80px;
+					margin:5px auto;					
+					margin-top:10px;
+			 
+			 }
+			.div5{
+			      width:40%;
+				  text-align:center;
+				  padding-left:400px;
+				  margin-top:-10px;
+				}
+			 .div6{
+				height:1px;
+				background-color:#fff;
+				
+			 }
+			 .div7{
+				text-align:center;
+				color:#27D3C3;
+				padding-top:10px;
+				
+			 }
+		</style>
+	</head>
+	<body style="background-color:#27D3C3">
+		<!-- Main Div -->
+		<div class="bgdiv">
+			<div class="div1 Divcommon"> 
+				<div class="div5"> Free Responsive Bootstrap Template </div>
+			</div>			
+			<div class="div2 Divcommon">
+				<div class="div3" > Website Template</div>
+				<div class="div4" ></div>
+			</div>
+			<!-- Body Div -->
+			<div>
+					<div class="subdiv1" >
+						<div class="subdiv10"></div>
+					</div>
+					
+					<div class="subdiv2">
+						<div class="subdiv3"> </div>
+						<div class="subdiv4"> </div>
+						<div class="subdiv5">
+							<div class="div6"></div>
+							<div class="subdiv9"></div>
+						</div>
+						<div class="subdiv6"></div>
+					</div>	
+					
+					<div class="subdiv1">
+						<div class="subdiv10"></div>
+					</div>
+			</div>
+			<!-- End of Body Div -->
+			<!-- Footer Div -->
+			<div>
+					<div class="subdiv7"></div>
+					<div class="subdiv7"></div>
+					<div class="subdiv7"></div>
+					<div class="subdiv8"><div class="div7"> template template template templatetemplate templatetemplate template</div></div>
+			</div>
+			<!-- End of Footer Div -->	
+		</div>
+		<!-- End of Main Div -->
+	</body>
+</html>
